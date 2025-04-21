b9bb953b52d7f44117c2828db8219f49e7092343
Chairoel <projectchairul@gmail.com>
2025-04-21 23:11:06 +0700

    update store

--- src/states/index.js
+++ src/states/index.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/reducer";
+import isPreLoadReducer from "./preload/reducer";
+import authUserReducer from "./auth/reducer";
 
 const store = configureStore({
   reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreLoadReducer,
     users: usersReducer,
   },
 });
