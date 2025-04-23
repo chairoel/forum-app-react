4a23efe28a7e369c92432943139d578f287e1714
Chairoel <projectchairul@gmail.com>
2025-04-23 12:00:14 +0700

    handle max length content Thread Body

--- src/components/ThreadBody.jsx
+++ src/components/ThreadBody.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../styles/thread.css";
 
-const ThreadBody = ({ body }) => {
-  return <p className="thread-body">{body}</p>;
-};
+const MAX_LENGTH = 268;
 
+const ThreadBody = ({ body }) => {
+  const truncatedBody =
+    body.length > MAX_LENGTH ? body.substring(0, MAX_LENGTH) + "..." : body; // Potong teks jika terlalu panjang
+
+  return (
+    <div
+      className="thread-body"
+      dangerouslySetInnerHTML={{ __html: truncatedBody }}
+    />
+  );
+};
 ThreadBody.propTypes = {
   body: PropTypes.string.isRequired,
 };
