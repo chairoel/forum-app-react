7827c9115f9a1982d8050670c18245a05dabcc8b
Chairoel <projectchairul@gmail.com>
2025-04-23 12:00:49 +0700

    update UI HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ThreadItem from "../components/ThreadItem";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncReceiveThreads } from "../states/threads/action";
+import ThreadTag from "../components/ThreadTag";
 
 const HomePage = ({ searchQuery }) => {
-  const data = [
-    { id: 1, title: "Diskusi React" },
-    { id: 2, title: "Diskusi Vue" },
-    { id: 3, title: "Diskusi Angular" },
-  ];
+  const { threads = [] } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  const filteredData = data.filter((item) =>
-    item.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  useEffect(() => {
+    dispatch(asyncReceiveThreads());
+  }, []);
+
+  const categories = [...new Set(threads.map((thread) => thread.category))];
+
+  const filteredData = threads.filter((item) => {
+    const isCategoryMatch = selectedCategory
+      ? item.category === selectedCategory
+      : true;
+    const isSearchMatch = item.title
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+    return isCategoryMatch && isSearchMatch;
+  });
+
+  const handleCategoryClick = (category) => {
+    if (selectedCategory === category) {
+      setSelectedCategory("");
+    } else {
+      setSelectedCategory(category);
+    }
+  };
+
+  const handleThreadClick = () => {
+    alert("Thread diklik!");
+  };
 
   return (
     <div>
-      <h1>Beranda</h1>
+      <div className="categories">
+        {categories.map((category) => (
+          <ThreadTag
+            key={category}
+            tag={category}
+            isSelected={selectedCategory === category}
+            onClick={() => handleCategoryClick(category)}
+          />
+        ))}
+      </div>
+
       <div>
         {filteredData.length > 0 ? (
-          filteredData.map((item) => <div key={item.id}>{item.title}</div>)
+          filteredData.map((item) => {
+            return (
+              <ThreadItem
+                key={item.id}
+                title={item.title}
+                tag={item.category}
+                body={item.body}
+                likes={item.upVotesBy.length}
+                dislikes={item.downVotesBy.length}
+                comments={item.totalComments}
+                createdBy={item.ownerId}
+                createdAt={item.createdAt}
+                onClick={handleThreadClick}
+              />
+            );
+          })
         ) : (
           <p>Data tidak ditemukan</p>
         )}
