5094e892349ef17df5904feb77958c7827520e91
Chairoel <projectchairul@gmail.com>
2025-04-23 12:04:45 +0700

    update UI time postAt

--- src/components/ThreadMeta.jsx
+++ src/components/ThreadMeta.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../styles/thread.css";
+import { postedAt } from "../utils/postedAt";
 
 const ThreadMeta = ({ createdBy, createdAt }) => {
   return (
     <div className="thread-meta">
       <span className="thread-author">{`Dibuat oleh ${createdBy}`}</span>
-      <span className="thread-time">{`${createdAt} hari lalu`}</span>
+      <span className="thread-time">{postedAt(createdAt)}</span>
     </div>
   );
 };
